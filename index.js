26333e6abe4a00b924a62470d2f83cad1d8dc5a9
Tony Ye <tony.zh.ye@gmail.com>
2015-12-02 12:30:21 +0800

    Init the repo

---
+++ index.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var Hoek = require('hoek');
+var fs = require('fs');
+
+exports.register = function (server, options, next) {
+  options = Hoek.applyToDefaults({
+    assetPath: path.join(process.cwd(), 'webpack-assets.json')
+    devMode: false
+  });
+
+  var manifest
+  var isManifestLoaded = false;
+
+  var loadManifest = function () {
+    try {
+      var data = JSON.parse(fs.readFileSync(options.assetPath, 'utf8'));
+      isManifestLoaded = true;
+      return data;
+    } catch (e) {
+      console.log('could not load asset manifest', e);
+    }
+  };
+
+  var webpack_asset = function (path) {
+    if (options.devMode || !isManifestLoaded) {
+      manifest = loadManifest();
+    }
+
+    if (manifest) {
+      return manifest[path];
+    } else {
+      return path;
+    }
+  };
+
+  server.method({
+    name: 'webpack_asset',
+    method: webpack_asset,
+    options: {}
+  });
+
+  return next();
+};
+
+export.register.attributes = {
+  pkg: require('./package.json')
+};
---
+++ readme.md
@@ -0,0 +1,25 @@
+hapi-webpack-asset
+===============
+
+Middleware to load hashed webpack assets, in combination with https://github.com/sporto/assets-webpack-plugin
+
+## Configuration
+
+Hapi config:
+
+```javascript
+```
+
+## Options
+
+```javascript
+```
+
+
+## Usage
+
+Hapi view helper:
+
+```
+```
+
